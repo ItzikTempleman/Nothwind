d0d408fedc2806cf03bbbb985e902c361fc0bc42
itzik templeman <itzik.templeman@gmail.com>
2025-06-23 10:14:22 +0300

    d

--- src/Utils/AppConfig.ts
+++ src/Utils/AppConfig.ts
@@ -2,6 +2,7 @@ class AppConfig {
 	//	public readonly productsUrl = "http://localhost:3030/delay/api/products/";
 	public readonly productsUrl = "http://localhost:3030/api/products/";
 	public readonly employeesUrl = "http://localhost:3030/api/employees/";
+	public readonly userUrl = "http://localhost:3030/api/register/";
 }
 
 export const appConfig = new AppConfig();
